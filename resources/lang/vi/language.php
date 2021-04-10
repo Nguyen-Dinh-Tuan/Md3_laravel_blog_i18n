2bf030b476b0326c41cf39ed34654f2c6f9483f0
tuan <tuandinh982002@gmail.com>
2021-04-10 09:40:18 +0700

    update

---
+++ resources/lang/vi/language.php
@@ -0,0 +1,11 @@
+<?php
+ return [
+     'blog_application' => 'Ung dung Blog',
+     'home' => 'Trang chu',
+     'list_post' => 'Danh sach bai viet',
+     'create_post' => 'Tao moi bai viet',
+     'save' => 'Luu',
+     'post_not_exit' => 'Khong co bai viet nao ',
+     'title' => 'Tieu de',
+     'content' => 'Noi dung',
+ ];
